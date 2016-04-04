8730fad92e6c242d0d51837816f47cf2ff6b57f6
koh110 <kohta110@gmail.com>
2016-04-05 02:24:05 +0900

    update version

--- dist/outerclick.js
+++ dist/outerclick.js
@@ -1,5 +1,5 @@
 /*
- * outerclick 1.0.0
+ * outerclick 1.0.1
  * outside click event of the element
  * This software is released under the MIT License
  * http://opensource.org/licenses/mit-license.php
